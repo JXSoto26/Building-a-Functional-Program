e1c621582d6236415598745d007d9c82f4e6c295
Jeremy Soto Flores <108308182+JXSoto26@users.noreply.github.com>
2024-02-17 11:35:59 -0300

    Add files via upload

---
+++ Building a Functional Program.js
@@ -0,0 +1,22 @@
+// Task 1: Build a function-based console log message generator
+function consoleStyler() {
+    console.log("Hello User");
+}
+
+// Task 2: Build another console log message generator
+function celebrateStyler() {
+    console.log("Congrats!");
+}
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+consoleStyler()
+celebrateStyler()
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate() {
+    consoleStyler()
+    celebrateStyler()
+}
+// Call styleAndCelebrate
+
+styleAndCelebrate()
